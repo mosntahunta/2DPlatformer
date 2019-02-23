fb3c3e809b5025db23943b6329b7a2fc90723bd5
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-02-24 02:25:00 +1300

    Add draft sprites for tiles and player Create the player object and the tilemap

---
+++ Mario/Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D myRigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
