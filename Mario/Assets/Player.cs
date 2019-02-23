9d46fc2ef080d311f0e93c0b2b3f50aae5381fe8
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-02-24 03:10:50 +1300

    Horizontal movement with input for the player

--- Mario/Assets/Player.cs
+++ Mario/Assets/Player.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float horizontalSpeed = 5.0f;
+
     Rigidbody2D myRigidBody2D;
 
     // Start is called before the first frame update
@@ -15,6 +18,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float direction = CrossPlatformInputManager.GetAxis("Horizontal");
+        myRigidBody2D.velocity = new Vector2(direction * horizontalSpeed, 0.0f);
     }
 }
