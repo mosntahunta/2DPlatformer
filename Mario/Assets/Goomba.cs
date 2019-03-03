ef9104204992ee79462e00d2fcbd6cfbdbda2ab5
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-03-04 00:52:10 +1300

    Goomba patrol movement

--- Mario/Assets/Goomba.cs
+++ Mario/Assets/Goomba.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class Goomba : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5.0f;
-    [SerializeField] float direction = -1.0f;
+    [SerializeField] float initialDirection = -1.0f;
 
     Rigidbody2D myRigidBody2D;
     Animator animator;
-    Vector2 scaleVector;
-
-    // Start is called before the first frame update
+    BoxCollider2D myBoxCollider2D;
+    GameObject player;
+    
     void Start()
     {
         myRigidBody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        scaleVector = new Vector2(0f, 0f);
-    }
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.localScale = new Vector2(initialDirection, 1f);
 
-    // Update is called once per frame
+        if (player && myBoxCollider2D)
+        {
+            foreach (var collider in player.GetComponentsInChildren<Collider2D>())
+            {
+                Physics2D.IgnoreCollision(collider, myBoxCollider2D);
+            }
+        }
+    }
+    
     void Update()
     {
         Run();
@@ -27,16 +36,25 @@ public class Goomba : MonoBehaviour
 
     private void Run()
     {
-        myRigidBody2D.velocity = new Vector2(direction * runSpeed, myRigidBody2D.velocity.y);
+        if (isFacingRight())
+        {
+            myRigidBody2D.velocity = new Vector2(runSpeed, myRigidBody2D.velocity.y);
+        }
+        else
+        {
+            myRigidBody2D.velocity = new Vector2(-runSpeed, myRigidBody2D.velocity.y);
+        }
         animator.SetBool("Running", true);
-        FlipSprite(direction);
     }
 
-    private void FlipSprite(float direction)
+    void OnTriggerExit2D(Collider2D collider)
     {
-        scaleVector.x = direction;
-        scaleVector.y = 1f;
+        float direction = Mathf.Sign(myRigidBody2D.velocity.x);
+        transform.localScale = new Vector2(-direction, 1f);
+    }
 
-        transform.localScale = scaleVector;
+    bool isFacingRight()
+    {
+        return transform.localScale.x > 0;
     }
 }
