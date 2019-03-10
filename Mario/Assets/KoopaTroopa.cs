31c97e21cccd86dd618cf8a422e2b6b1a3a870c0
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-03-10 22:47:26 +1300

    Basic Koopa model and run animation

---
+++ Mario/Assets/KoopaTroopa.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoopaTroopa : MonoBehaviour
+{
+    // State
+    bool isAlive = true;
+
+    Rigidbody2D myRigidBody2D;
+    Animator animator;
+    CapsuleCollider2D myCapsuleCollider2D;
+    GameObject player;
+    Patrol patrol;
+
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        myCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        patrol = GetComponent<Patrol>();
+    }
+
+    void Update()
+    {
+        if (!isAlive) { return; }
+
+        patrol.Proceed();
+    }
+
+    // handle death when colliding with the player's feet
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //isAlive = false;
+        //Rigidbody2D player_rigidbody = player.GetComponent<Rigidbody2D>();
+        //player_rigidbody.velocity = new Vector2(player_rigidbody.velocity.x, 15.0f);
+        //myRigidBody2D.velocity = new Vector2(0.0f, 0.0f);
+        //animator.SetTrigger("Dying");
+        //foreach (var collider in player.GetComponentsInChildren<Collider2D>())
+        //{
+        //    Physics2D.IgnoreCollision(collider, myCapsuleCollider2D);
+        //}
+        //Destroy(gameObject, 1);
+    }
+}
