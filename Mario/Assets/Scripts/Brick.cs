e33191d6c59415d687e396e648fd5bd85f4fb578
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-03-20 00:06:33 +1300

    Implement the brick break feature

---
+++ Mario/Assets/Scripts/Brick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public GameObject destroyedBricks;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector2 contactPoint = collision.GetContact(0).point;
+            Vector2 center = collision.collider.bounds.center;
+
+            float minX = collision.collider.bounds.min.x;
+            float maxX = collision.collider.bounds.max.x;
+
+            if (contactPoint.y > center.y && contactPoint.x > minX && contactPoint.x < maxX)
+            {
+                GameObject obj = Instantiate(destroyedBricks, transform.position, transform.rotation);
+                Destroy(gameObject);
+                Destroy(obj, 2);
+            }
+        }
+    }
+}
---
+++ Mario/Assets/Scripts/BrickDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickDestroy : MonoBehaviour
+{
+    public Vector2 initialVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = initialVelocity;
+    }
+}
