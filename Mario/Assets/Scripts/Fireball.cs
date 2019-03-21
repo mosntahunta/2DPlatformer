44b6ba9e10af016fa7159ae39b5d048f518e1272
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-03-22 03:04:48 +1300

    Apply reflection to the fireball only for ground surfaces

--- Mario/Assets/Scripts/Fireball.cs
+++ Mario/Assets/Scripts/Fireball.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    Rigidbody2D myRigidBody2D;
+    Vector2 previousVelocity;
 
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        previousVelocity = myRigidBody2D.velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // get the point of contact
+        ContactPoint2D contact = collision.GetContact(0);
+
+        // reflect our old velocity off the contact point's normal vector
+        Vector2 reflectedVelocity = Vector2.Reflect(previousVelocity, contact.normal);
+
+        // assign the reflected velocity back to the rigidbody
+        myRigidBody2D.velocity = reflectedVelocity;
+
+        // rotate the object by the same ammount we changed its velocity
+        Quaternion rotation = Quaternion.FromToRotation(previousVelocity, reflectedVelocity);
+        transform.rotation = rotation * transform.rotation;
+
+        print(reflectedVelocity.x);
+        if (Mathf.Sign(previousVelocity.x) != Mathf.Sign(reflectedVelocity.x))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
