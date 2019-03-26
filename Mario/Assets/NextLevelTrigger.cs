214cc9d574cb73b53a3d16483ff92c888af48b2b
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-03-26 22:19:28 +1300

    Add a game state manager and the next level trigger
    Created a new scene to test this

---
+++ Mario/Assets/NextLevelTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelTrigger : MonoBehaviour
+{
+    public LevelManager levelManager;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            levelManager.LoadNextScene();
+        }
+    }
+}
