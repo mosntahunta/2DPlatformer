b16e314694ee9b905644daea91b6ac3e48ff293e
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-05-01 23:55:14 +1200

    Implement the jump input buffering * so that the player can jump even if the jump button is pressed before landing

--- Mario/Assets/Scripts/PlayerController.cs
+++ Mario/Assets/Scripts/PlayerController.cs
@@ -10,9 +10,11 @@ public class PlayerController : PhysicsObject
     public float jumpCancelFactor = 0.6f;
 
     public float jumpGraceTime = 0.2f;
+    public float jumpInputBufferTime = 0.2f;
 
     private float jumpGraceTimer;
-    
+    private float jumpInputBufferTimer;
+
     void Start()
     {
         
@@ -27,14 +29,25 @@ public class PlayerController : PhysicsObject
         if (grounded)
         {
             jumpGraceTimer = jumpGraceTime;
+
+            if (jumpInputBufferTimer > 0)
+            {
+                Jump();
+            }
         }
         else if (jumpGraceTimer > 0)
         {
             jumpGraceTimer -= Time.deltaTime;
         }
 
+        if (jumpInputBufferTimer > 0)
+        {
+            jumpInputBufferTimer -= Time.deltaTime;
+        }
+
         if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
+            jumpInputBufferTimer = jumpInputBufferTime;
             if (jumpGraceTimer > 0)
             {
                 Jump();
@@ -54,6 +67,7 @@ public class PlayerController : PhysicsObject
     private void Jump()
     {
         jumpGraceTimer = 0;
+        jumpInputBufferTimer = 0;
         velocity.y = jumpSpeedY;
     }
 }
