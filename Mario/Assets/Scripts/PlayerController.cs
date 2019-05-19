c4fcaeb19da051ff380d3e9b5a3df2655deeaad6
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-05-19 18:19:21 +1200

    Add `Dash` state and horizontal speed boost for jumps
    * Also added SuperJump, which occurs when jumping wihle dashing

--- Mario/Assets/Scripts/PlayerController.cs
+++ Mario/Assets/Scripts/PlayerController.cs
@@ -10,21 +10,34 @@ public class PlayerController : PhysicsObject
     public float drag = 0.12f;
     
     public float horizontalAcceleration = 1000f;
+    public float horizontalDeceleration = 400f;
     public float maxHorizontalSpeed = 3f;
     public float minimumHorizontalSpeed = 0.1f;
-    private float horizontalSpeed = 0.0f;
+    public float horizontalSpeed = 0.0f;
 
-    public float platformCameraMovementRange = 1f;
+    private float moveX = 0f;
 
     public float jumpSpeedY = 7f;
+    public float superJumpSpeedY = 10f;
 
     public float jumpGraceTime = 0.2f;
     public float jumpInputBufferTime = 0.2f;
     public float jumpVarTime = 0f;
 
+    public float jumpHBoost = 10f;
+    public float superJumpH = 50f;
+
     private float jumpGraceTimer;
     private float jumpInputBufferTimer;
     private float jumpVarTimer = 0f;
+    
+
+    public float dashSpeedX = 20f;
+    public float dashTime = 0.15f;
+    public float dashCooldownTime = 0.2f;
+    private float dashCooldownTimer = 0f;
+
+    public float platformCameraMovementRange = 1f;
 
     private float lastLandedPlatformHeight = float.NegativeInfinity;
 
@@ -48,26 +61,55 @@ public class PlayerController : PhysicsObject
         stateMachine.SetState(normalState);
     }
 
+    void Update()
+    {
+        if (dashCooldownTimer > 0)
+        {
+            dashCooldownTimer -= Time.deltaTime;
+        }
+        
+    }
+
     // Normal state
     private int NormalUpdate()
     {
         targetVelocity = Vector2.zero;
-        Vector2 move = Vector2.zero;
+        moveX = 0;
 
+        // Horizontal Movement
         if (CrossPlatformInputManager.GetButton("Horizontal"))
         {
-            move.x = Mathf.Sign(CrossPlatformInputManager.GetAxis("Horizontal"));
+            moveX = Mathf.Sign(CrossPlatformInputManager.GetAxis("Horizontal"));
+        }
+        
+        // approach the max speed with acceleration or deceleration
+        if (Mathf.Abs(horizontalSpeed) > maxHorizontalSpeed && Mathf.Sign(horizontalSpeed) == moveX)
+        {
+            horizontalSpeed = Mathf.MoveTowards(horizontalSpeed, moveX * maxHorizontalSpeed, horizontalDeceleration * Time.deltaTime);
         }
         else
         {
-            move.x = 0f;
+            horizontalSpeed = Mathf.MoveTowards(horizontalSpeed, moveX * maxHorizontalSpeed, horizontalAcceleration * Time.deltaTime);
         }
+        
+        // stop if speed is below the minimum horizontal speed
+        if (Mathf.Abs(horizontalSpeed) <= minimumHorizontalSpeed) horizontalSpeed = 0.0f;
 
-        if (CrossPlatformInputManager.GetButton("Fire1"))
+        // face the correct way
+        if (horizontalSpeed != 0 && facingDirection != Mathf.Sign(horizontalSpeed))
+        {
+            facingDirection = Mathf.Sign(horizontalSpeed);
+        }
+        
+        // Dashing
+        if (CanDash)
         {
             return dashState;
         }
 
+        ApplyGravity();
+
+        // Jumping
         if (grounded)
         {
             jumpGraceTimer = jumpGraceTime;
@@ -86,9 +128,7 @@ public class PlayerController : PhysicsObject
         {
             jumpInputBufferTimer -= Time.deltaTime;
         }
-
-        ApplyGravity();
-
+        
         // variable jumping
         if (jumpVarTimer > 0)
         {
@@ -112,24 +152,6 @@ public class PlayerController : PhysicsObject
                 Jump();
             }
         }
-        
-        // apply horizontal acceleration over time
-        horizontalSpeed = Mathf.MoveTowards(horizontalSpeed, move.x * maxHorizontalSpeed, horizontalAcceleration * Time.deltaTime);
-
-        // apply drag
-        horizontalSpeed = Mathf.Lerp(horizontalSpeed, 0, drag);
-
-        // stop if speed is below the minimum horizontal speed
-        if (Mathf.Abs(horizontalSpeed) <= minimumHorizontalSpeed) horizontalSpeed = 0.0f;
-
-        // face the correct way
-        if (facingDirection != Mathf.Sign(horizontalSpeed))
-        {
-            facingDirection = Mathf.Sign(horizontalSpeed);
-        }
-
-        // limit the speed
-        horizontalSpeed = Mathf.Min(Mathf.Abs(horizontalSpeed), maxHorizontalSpeed) * facingDirection;
 
         targetVelocity.x = horizontalSpeed;
 
@@ -137,27 +159,66 @@ public class PlayerController : PhysicsObject
     }
 
     // Dash state
+    public bool CanDash
+    {
+        get
+        {
+            return CrossPlatformInputManager.GetButtonDown("Dash") && dashCooldownTimer <= 0 && jumpGraceTimer > 0;
+        }
+    }
+
     private void DashBegin()
     {
-        //Debug.Log("Dash begin");
+        dashCooldownTimer = dashCooldownTime;
     }
 
     private void DashExit()
     {
-        //Debug.Log("Dash exit");
     }
 
     private int DashUpdate()
     {
-        //Debug.Log("Dash update");
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            SuperJump();
+            return normalState;
+        }
+
         return dashState;
     }
 
     private IEnumerator DashCoroutine()
     {
-        //Debug.Log("Dash Coroutine");
         yield return null;
+
+        // todo: perhaps use the aim vector to determine dash direction, rather than facing
+        targetVelocity.x = dashSpeedX * facingDirection;
+
+        yield return new WaitForSeconds(dashTime);
+
         stateMachine.SetState(normalState);
+    }
+
+    private void Jump()
+    {
+        jumpGraceTimer = 0;
+        jumpInputBufferTimer = 0;
+        jumpVarTimer = jumpVarTime;
+
+        velocity.y = jumpSpeedY;
+
+        horizontalSpeed += moveX * jumpHBoost;
+    }
+
+    private void SuperJump()
+    {
+        jumpGraceTimer = 0;
+        jumpInputBufferTimer = 0;
+        jumpVarTimer = jumpVarTime;
+
+        velocity.y = superJumpSpeedY;
+
+        horizontalSpeed = facingDirection * superJumpH;
     }
 
     protected override void OnLanded(int layer)
@@ -189,7 +250,7 @@ public class PlayerController : PhysicsObject
         {
             if (targetVelocity.x != 0.0f)
             {
-                bool isMaxSpeed = Mathf.Abs(targetVelocity.x) == Mathf.Lerp(maxHorizontalSpeed, 0, drag);
+                bool isMaxSpeed = Mathf.Abs(targetVelocity.x) == maxHorizontalSpeed;
                 cameraController.SetHorizontalTarget(rb2d.position, facingDirection, isMaxSpeed);
             }
 
@@ -207,14 +268,5 @@ public class PlayerController : PhysicsObject
             }
             cameraController.SetPlayerIsMoving(targetVelocity.x != 0.0f);
         }
-    }
-
-    private void Jump()
-    {
-        jumpGraceTimer = 0;
-        jumpInputBufferTimer = 0;
-        jumpVarTimer = jumpVarTime;
-
-        velocity.y = jumpSpeedY;
     }
 }
