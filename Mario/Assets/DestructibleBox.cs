a1e395e69283b9c946c38d6ba17ede1dfb7aeb0c
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-06-02 19:22:30 +1200

    Add the bouncing ball and destructible box scripts

---
+++ Mario/Assets/DestructibleBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleBox : HittableObject
+{
+    protected override void OnHit(float damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
