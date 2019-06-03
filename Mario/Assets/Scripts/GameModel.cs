7da45dd161553edd90d4f9d5e653414affe51adc
LYLEntertainment <42032961+LYLEntertainment@users.noreply.github.com>
2019-06-03 15:15:11 +1200

    Refactored the game model to have 2 models (player and game)

--- Mario/Assets/Scripts/GameModel.cs
+++ Mario/Assets/Scripts/GameModel.cs
@@ -11,13 +11,19 @@ public class GameModel : MonoBehaviour
 {
     public static GameModel SharedInstance;
     public PlayerData playerData;
+    public GameData gameData;
 
     [Serializable]
     public struct PlayerData
     {
         public int currentLives;
         public int maxLives;
-        public int currentLevelIndex;
+    }
+
+    [Serializable]
+    public struct GameData
+    {
+        public int currentSceneIndex;
     }
 
     void Awake()
@@ -30,7 +36,8 @@ public class GameModel : MonoBehaviour
 
             SharedInstance.playerData.currentLives = playerData.currentLives;
             SharedInstance.playerData.maxLives = playerData.maxLives;
-            SharedInstance.playerData.currentLevelIndex = playerData.currentLevelIndex;
+
+            SharedInstance.gameData.currentSceneIndex = gameData.currentSceneIndex;
         }
         else if (SharedInstance != this)
         {
@@ -42,57 +49,40 @@ public class GameModel : MonoBehaviour
     public void SaveData()
     {
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
-        
-        bf.Serialize(file, playerData);
+
+        CreateSaveFile<PlayerData>("/playerInfo.dat", playerData, bf);
+        CreateSaveFile<GameData>("/gameInfo.dat", gameData, bf);
+    }
+
+    private void CreateSaveFile<T>(string fileName, T data, BinaryFormatter bf )
+    {
+        FileStream file = File.Create(Application.persistentDataPath + fileName);
+        bf.Serialize(file, data);
         file.Close();
     }
 
     // load data from a saved file
     public void LoadData()
     {
+        BinaryFormatter bf = new BinaryFormatter();
+
         if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
         {
-            BinaryFormatter bf = new BinaryFormatter();
             FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
             PlayerData data = (PlayerData)bf.Deserialize(file);
             file.Close();
 
             playerData.currentLives = data.currentLives;
             playerData.maxLives = data.maxLives;
-            playerData.currentLevelIndex = data.currentLevelIndex;
         }
-    }
 
-    public void LoadNextLevel()
-    {
-        StartCoroutine(LoadYourAsyncScene(playerData.currentLevelIndex));
-    }
-
-    public void PrepareForLoadingSavedLevel()
-    {
-        LoadData();
-    }
-
-    public void PrepareForTransitionToNextLevel()
-    {
-        playerData.currentLevelIndex++;
-        SaveData();
-    }
-
-    IEnumerator LoadYourAsyncScene(int sceneIndex)
-    {
-        // The Application loads the Scene in the background as the current Scene runs.
-        // This is particularly good for creating loading screens.
-        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
-        // a sceneBuildIndex of 1 as shown in Build Settings.
-
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
-
-        // Wait until the asynchronous scene fully loads
-        while (!asyncLoad.isDone)
+        if (File.Exists(Application.persistentDataPath + "/gameInfo.dat"))
         {
-            yield return null;
+            FileStream file = File.Open(Application.persistentDataPath + "/gameInfo.dat", FileMode.Open);
+            GameData data = (GameData)bf.Deserialize(file);
+            file.Close();
+
+            gameData.currentSceneIndex = data.currentSceneIndex;
         }
     }
 }
